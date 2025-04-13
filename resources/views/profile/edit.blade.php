bf52307de7bf256b5f1e193ba5bc8462b9e04172
lecongchuc <0l14l301@gmail.com>
2025-04-13 21:00:42 +0700

    add success or error message when update profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -2,6 +2,23 @@
 @section('title', 'Hồ sơ cá nhân')
 
 @section('content')
+<div class="container-fluid py-4">
+    @if(session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    @if(session('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('error') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    @yield('content')
+</div>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-6">
